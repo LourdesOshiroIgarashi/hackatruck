9cf2c0617c5242c90cd834b6f1beecfb915709e0
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-06-08 00:39:21 -0400

    Create 01.swift

---
+++ Hackatruck/Conceitos e Fundamentos: SWIFT (Algoritmos, Orientação a Objetos)/Algoritmos/1.1 Conceitos Iniciais/1.1.2 Padrões de Escrita e Nomenclaturas/01.swift
@@ -0,0 +1,13 @@
+/*
+    Segundo a convenção da documentação oficial do Swift nomes de tipos (classes) e protocolos são UpperCamelCase. 
+    Todo o restante é lowerCamelCase. 
+    O Camel Case é uma forma de agrupar e combinar palavras de forma legível e sem espaços para validar a nossa regra de nomenclatura.
+    
+    Nome Completo
+    user login count
+    
+*/
+
+var nomeCompleto = ""
+
+var userLoginCount = ""
