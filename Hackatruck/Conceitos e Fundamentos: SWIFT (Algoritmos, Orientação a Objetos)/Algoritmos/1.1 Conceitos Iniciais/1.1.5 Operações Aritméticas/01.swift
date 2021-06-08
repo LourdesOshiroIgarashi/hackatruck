08bbeb139b29bd5edf9be7c76dde92847442352d
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-06-08 00:49:07 -0400

    Create 01.swift

---
+++ Hackatruck/Conceitos e Fundamentos: SWIFT (Algoritmos, Orientação a Objetos)/Algoritmos/1.1 Conceitos Iniciais/1.1.5 Operações Aritméticas/01.swift
@@ -0,0 +1,15 @@
+var x: Int = 10
+var y: Int = 2
+
+
+var total: Int 
+total = x + y
+print("Resultado - Adição de " + String(x) + " + " + String(y) + " é " + String(total))
+
+
+total = x - y
+print("Resultado - Subtração de " + String(x) + " - " + String(y) + " é " + String(total))
+
+
+total = x * y
+print("Resultado - Multiplicação de " + String(x) + " * " + String(y) + " é " + String(total))
